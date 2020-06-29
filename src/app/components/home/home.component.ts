c134d12ed69c85550503a94e0f3729d08e003b79
miguel_casas <miguel.casas@atmira.com>
2020-06-29 19:44:41 +0200

    Eventos del ciclo de vida

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,30 +1,73 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnChanges, DoCheck, AfterContentInit,
+         AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   template: `
 
     <app-ng-style></app-ng-style>
+    <br>
 
     <app-css></app-css>
+    <br>
 
     <app-clases></app-clases>
+    <br><br/>
 
     <p [appResaltado]="'red'">
         Hola Mundo
     </p>
 
+    <br>
     <app-ng-switch></app-ng-switch>
 
   `,
   styles: [
   ]
 })
-export class HomeComponent implements OnInit {
 
-  constructor() { }
+
+export class HomeComponent implements OnInit, OnChanges, DoCheck, AfterContentInit,
+AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy {
+
+
+  constructor() {
+    console.log("constructor");
+  }
+
+  // Los que más se usan son ngOnInit, ngOnChanges y ngOnDestroy
 
   ngOnInit(): void {
+    console.log("ngOnInit");
   }
+
+  ngOnChanges(){
+    console.log("ngOnChanges");
+  }
+
+  ngDoCheck(){
+    console.log("ngDoCheck");
+  }
+  
+  ngAfterContentInit(){
+    console.log("ngAfterContentInit");
+  }
+  
+  ngAfterContentChecked(){
+    console.log("ngAfterContentChecked");
+  }
+  
+  ngAfterViewInit(){
+    console.log("ngAfterViewInit");
+  }
+  
+  ngAfterViewChecked(){
+    console.log("ngAfterViewChecked");
+  }
+  
+  ngOnDestroy(){
+    console.log("ngOnDestroy");
+  }
+  
 
 }
